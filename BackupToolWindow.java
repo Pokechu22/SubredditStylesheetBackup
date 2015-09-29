4d058ec47ab0d3db168380765e5cc6b870ea67fb
Pokechu22 <Pokechu022@gmail.com>
2015-09-29 06:59:17 -0700

    A bit more UI stuff.

--- BackupToolWindow.java
+++ BackupToolWindow.java
@@ -13,6 +13,7 @@ import javax.swing.JSeparator;
 import javax.swing.JList;
 import javax.swing.JSplitPane;
 import javax.swing.JButton;
+import javax.swing.JProgressBar;
 
 
 public class BackupToolWindow {
@@ -128,10 +129,10 @@ public class BackupToolWindow {
 		JPanel panel_1 = new JPanel();
 		splitPane.setRightComponent(panel_1);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{0, 0, 0};
-		gbl_panel_1.rowHeights = new int[]{0, 0, 0, 0};
-		gbl_panel_1.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.columnWidths = new int[]{252, 0, 0};
+		gbl_panel_1.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_1.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel_1.setLayout(gbl_panel_1);
 		
 		JLabel lblSaveLocation = new JLabel("Save location");
@@ -157,6 +158,42 @@ public class BackupToolWindow {
 		gbc_btnNewButton.gridx = 1;
 		gbc_btnNewButton.gridy = 1;
 		panel_1.add(btnNewButton, gbc_btnNewButton);
+		
+		JSeparator separator_2 = new JSeparator();
+		GridBagConstraints gbc_separator_2 = new GridBagConstraints();
+		gbc_separator_2.insets = new Insets(0, 0, 5, 0);
+		gbc_separator_2.gridwidth = 2;
+		gbc_separator_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_separator_2.gridx = 0;
+		gbc_separator_2.gridy = 2;
+		panel_1.add(separator_2, gbc_separator_2);
+		
+		JLabel lblProgress = new JLabel("Progress");
+		GridBagConstraints gbc_lblProgress = new GridBagConstraints();
+		gbc_lblProgress.insets = new Insets(0, 0, 5, 0);
+		gbc_lblProgress.gridwidth = 2;
+		gbc_lblProgress.gridx = 0;
+		gbc_lblProgress.gridy = 3;
+		panel_1.add(lblProgress, gbc_lblProgress);
+		
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setString("CurrentTask");
+		progressBar.setStringPainted(true);
+		GridBagConstraints gbc_progressBar = new GridBagConstraints();
+		gbc_progressBar.insets = new Insets(0, 0, 5, 0);
+		gbc_progressBar.fill = GridBagConstraints.HORIZONTAL;
+		gbc_progressBar.gridwidth = 2;
+		gbc_progressBar.gridx = 0;
+		gbc_progressBar.gridy = 4;
+		panel_1.add(progressBar, gbc_progressBar);
+		
+		JSeparator separator_3 = new JSeparator();
+		GridBagConstraints gbc_separator_3 = new GridBagConstraints();
+		gbc_separator_3.gridwidth = 2;
+		gbc_separator_3.insets = new Insets(0, 0, 0, 5);
+		gbc_separator_3.gridx = 0;
+		gbc_separator_3.gridy = 5;
+		panel_1.add(separator_3, gbc_separator_3);
 	}
 
 }
